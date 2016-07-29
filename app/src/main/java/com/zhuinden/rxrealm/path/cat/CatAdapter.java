94b8d0da83cc008148cb47412bf61ddddbb63194
Zhuinden <zhuinden@gmail.com>
2016-07-29 15:42:13 +0200

    open cat in browser when clicked

--- app/src/main/java/com/zhuinden/rxrealm/path/cat/CatAdapter.java
+++ app/src/main/java/com/zhuinden/rxrealm/path/cat/CatAdapter.java
@@ -6,8 +6,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.zhuinden.rxrealm.R;
 
 import io.realm.OrderedRealmCollection;
@@ -33,9 +31,6 @@ public class CatAdapter
     @Override
     public void onBindViewHolder(CatViewHolder holder, int position) {
         Cat cat = getItem(position);
-        if(cat != null) {
-            Glide.with(context).load(cat.getUrl()).diskCacheStrategy(DiskCacheStrategy.ALL).into(holder.image);
-            holder.sourceUrl.setText(cat.getSourceUrl());
-        }
+        holder.bind(context, cat);
     }
 }
--- app/src/main/java/com/zhuinden/rxrealm/path/cat/CatViewHolder.java
+++ app/src/main/java/com/zhuinden/rxrealm/path/cat/CatViewHolder.java
@@ -1,20 +1,38 @@
 package com.zhuinden.rxrealm.path.cat;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.zhuinden.rxrealm.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import flowless.ActivityUtils;
 
 /**
  * Created by Zhuinden on 2016.07.29..
  */
 public class CatViewHolder
         extends RecyclerView.ViewHolder {
+    private String _sourceUrl;
+
+    @OnClick(R.id.cat_item_container)
+    public void openCat(View view) {
+        if(_sourceUrl != null) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(_sourceUrl));
+            ActivityUtils.getActivity(view.getContext()).startActivity(intent);
+        }
+    }
+
     @BindView(R.id.cat_image)
     ImageView image;
 
@@ -24,5 +42,18 @@ public class CatViewHolder
     public CatViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
+    }
+
+    public void bind(Context context, Cat cat) {
+        if(cat != null) {
+            Glide.with(context).load(cat.getUrl()).diskCacheStrategy(DiskCacheStrategy.ALL).into(image);
+            sourceUrl.setText(cat.getSourceUrl());
+            _sourceUrl = cat.getSourceUrl();
+        } else {
+            Glide.clear(image);
+            sourceUrl.setText("");
+            _sourceUrl = null;
+        }
+
     }
 }
