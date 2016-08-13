1d0931dc73cbcdd5f346a4896c8d522775d0ac89
Zhuinden <zhuinden@gmail.com>
2016-08-13 17:16:04 +0200

    remove clutter from subscriber

--- app/src/main/java/com/zhuinden/rxrealm/util/RecyclerViewScrollBottomOnSubscribe.java
+++ app/src/main/java/com/zhuinden/rxrealm/util/RecyclerViewScrollBottomOnSubscribe.java
@@ -24,24 +24,9 @@ public final class RecyclerViewScrollBottomOnSubscribe
         final RecyclerView.OnScrollListener watcher = new RecyclerView.OnScrollListener() {
             @Override
             public final void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                if(!recyclerView.canScrollVertically(-1)) {
-                    onScrolledToTop();
-                } else if(!recyclerView.canScrollVertically(1)) {
+                if(!recyclerView.canScrollVertically(1)) {
                     onScrolledToBottom();
-                } else if(dy < 0) {
-                    onScrolledUp();
-                } else if(dy > 0) {
-                    onScrolledDown();
                 }
-            }
-
-            public void onScrolledUp() {
-            }
-
-            public void onScrolledDown() {
-            }
-
-            public void onScrolledToTop() {
             }
 
             public void onScrolledToBottom() {
